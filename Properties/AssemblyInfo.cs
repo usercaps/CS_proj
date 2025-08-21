1deef11116ce92265f93874ef627742bb766a5ae
user_caps <krevedko18@gmail.com>
2025-08-21 13:54:05 +0300

    first com

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TitleGen")]
+[assembly: AssemblyDescription("Программа для генерации протоколов")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Custom")]
+[assembly: AssemblyProduct("TitleGen")] 
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c1b13e5e-3f71-4f27-8a1a-92dc8b771234")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
