64c5197f6e5e885ffe373c97ac068dd3cf2b9772
user_caps <krevedko18@gmail.com>
2025-08-22 09:17:48 +0300

    изменил плейсхолдеры на русский. и вроде перенес шаблон в другую папку.

--- MainForm.cs
+++ MainForm.cs
@@ -205,7 +205,8 @@ namespace TitleGen
                 doc = wordApp.Documents.Open(path, ReadOnly: true, Visible: false);
                 string text = doc.Content.Text;
 
-                var matches = Regex.Matches(text, @"\{(.+?)\}");
+                var matches = Regex.Matches(text, @"\{([Р-пр-џA-Za-z0-9_]+)\}");
+
                 foreach (Match match in matches)
                 {
                     string ph = match.Groups[1].Value;
